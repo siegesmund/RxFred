329fe56ddcd063a69754432e7322972e2b634a41
Peter Siegesmund <peter.siegesmund@icloud.com>
2019-07-29 09:01:15 -0400

    cleanup Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "rxFred",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "rxFred",
             targets: ["rxFred"]),
@@ -16,8 +15,6 @@ let package = Package(
         .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "rxFred",
             dependencies: ["fred", "RxSwift"]),
